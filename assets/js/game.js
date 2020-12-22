254496adcdff9f42a1a49a948030e763aef73f88
gwarz <gmwarzecha@tutanota.com>
2020-12-21 18:21:07 -0700

    added robot health updates

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,20 +1,48 @@
 
-// this creates a function named "fight"
-
-function fight() {
-    window.alert("The fight has begun!")
-}
 
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
+var enemyName = "Roborto";
+var enemyHealth = 50; 
+var enemyAttack = 12;
 
-console.log(playerName);
+var fight = function() {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
 
-console.log("This logs a string, good for leaving yourself a message")
+    //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in 'enemyHealth' variable
+    enemyHealth = enemyHealth - playerAttack;
+    //Log a resulting message to the console so we know that it worked. 
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
 
-console.log(10 + 10);
+    //check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.")
+    }
+    //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that results to update the value in the 'playerHealth' variable
+    playerHealth = playerHealth - enemyAttack;
+    //Log a resulting message to the console so we know that it worked. 
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
 
-console.log("Our robot's name is " + playerName)
+    // check player's health
+    if (playerHealth <=0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 
+}
 
-// fight();
+console.log(playerName, playerAttack, playerHealth, enemyName, enemyHealth, enemyAttack);
+
+fight()
